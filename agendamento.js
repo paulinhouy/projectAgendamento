83c25b4fbaefc7f22fb854be0f0ea8d447600c08
Paulo <paulofilhorocha22@gmail.com>
2025-08-13 19:02:06 -0300

    first commit

--- agendamento.js
+++ agendamento.js
@@ -2,4 +2,9 @@ const doctors = ['Dr.João',"Dra.Maria", 'Dr. Pedro'];
 let appointments = JSON.parse(localStorage.getItem('appointments') || '[]');
 
 const doctorFilter = document.getElementById('doctorFilter');
-const searchInput = document.getElementById('searchFilter')
+const searchInput = document.getElementById('searchFilter');
+const calendar = document.getElementById('calendar');
+const modal = document.getElementById('modal');
+const deleteBtn = document.getElementById('deleteBtn');
+const appointmentDoctor = document.getElementById('appointmentDoctor');
+
